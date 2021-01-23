b4a1e390985929b18150b355e846875b021b282d
Justin Sienicki <justinsienicki@att.net>
2021-01-23 13:35:22 -0600

    Added Admin and FileReader classes

---
+++ PayrollApp/PayrollApp/Admin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayrollApp
+{
+    class Admin : Staff
+    {
+        private const float overtimeRate = 15.5F;
+        private const float adminHourlyRate = 30F;
+
+        public float Overtime { get; private set; }
+
+        public Admin(string name) : base(name, adminHourlyRate){}
+
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+
+            if(HoursWorked > 160)
+            {
+                Overtime = overtimeRate * (HoursWorked - 160);
+                TotalPay = BasicPay + Overtime;
+            }
+        }
+        public override string ToString()
+        {
+            return "\nNameOfStaff = " + NameOfStaff +
+                "\nadminHourlyRate = " + adminHourlyRate +
+                "\nHoursWorked = " + HoursWorked +
+                "\nBasicPay = " + BasicPay +
+                "\nOvertime = " + Overtime +
+                "\n\nTotalPay = " + TotalPay;
+        }
+
+    }
+}
